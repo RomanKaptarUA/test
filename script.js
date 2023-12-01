384f25683f89be3f4217789afcdc91f49b30c21c
RomanKaptarUA <romankaptar82@gmail.com>
2023-12-01 20:29:32 +0200

    ua

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-(() => { 
-    const refs = { 
-      openModalBtn: document.querySelector("[data-modal-open]"), 
-      closeModalBtn: document.querySelector("[data-modal-close]"), 
-      modal: document.querySelector("[data-modal]"), 
-    }; 
-   
-    refs.openModalBtn.addEventListener("click", toggleModal); 
-    refs.closeModalBtn.addEventListener("click", toggleModal); 
-   
-    function toggleModal() { 
-      refs.modal.classList.toggle("is-hidden"); 
-      document.body.classList.toggle("no-scroll"); 
-    } 
-  })();
+  AOS.init();
 
   
